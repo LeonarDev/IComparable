47f877769c409faa921b7b4765926e35f14764eb
LeonarDev <leonardo.majevski@gmail.com>
2021-09-13 19:30:43 -0300

    solve exercise

--- IComparable/Program.cs
+++ IComparable/Program.cs
@@ -8,7 +8,32 @@ namespace IComparable
     {
         static void Main(string[] args)
         {
-            
+            string path = @"C:\Users\Majevski\Documents\Dev\github\IComparable\IComparable\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    
+                    list.Sort();
+
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
