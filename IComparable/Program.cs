219b8548ba8db533d29e86568cc2e78768634f2f
LeonarDev <leonardo.majevski@gmail.com>
2021-09-13 19:48:32 -0300

    solve exercise 2

--- IComparable/Program.cs
+++ IComparable/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Collections.Generic;
+using IComparables.Entities;
 
-namespace IComparable
+namespace IComparables
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // EXERCISE 1
             string path = @"C:\Users\Majevski\Documents\Dev\github\IComparable\IComparable\in.txt";
 
             try
@@ -26,6 +28,35 @@ namespace IComparable
                     foreach (string str in list)
                     {
                         Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine();
+            // EXERCISE 2
+            string path2 = @"C:\Users\Majevski\Documents\Dev\github\IComparable\IComparable\in2.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path2))
+                {
+                    List<Employee> list = new List<Employee>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
                     }
                 }
             }
